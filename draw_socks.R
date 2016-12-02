473e2bb23a950b6883d075923284aabcf8e75612
dpwynne <dpwynne@gmail.com>
2016-12-01 23:43:31 -0800

    Add files via upload
    draw_socks: R file for Part 4

---
+++ draw_socks.R
@@ -0,0 +1,27 @@
+##Define the function draw.socks, with inputs nblue and nred
+##where nblue is the number of blue socks and nred is the number of red socks
+function(){
+
+  ##define a character vector, sock.colors
+  ##containing "blue" and "red"
+
+  ##define a numeric vector, sock.numbers
+  ##containing nblue and nred
+
+  
+  sock.drawer <- rep(sock.colors, sock.numbers)
+  ##creates nblue "blue" and nred "red" socks
+  
+  ##sample 2 socks from the sock drawer without replacement
+  ##store the result in the variable sock.pair
+
+  ##print the colors of the drawn socks
+
+  ##if a match exists, print that you have a match
+  ##otherwise, print that you don't
+
+  ##return the pair of socks
+  return()
+}
+
+##run your function and store the result in the variable socks
